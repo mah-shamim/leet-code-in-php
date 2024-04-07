64aed60ef3d7f78a717e2db538512284369418be
MD ARIFUL HAQUE <mah.shamim@gmail.com>
2024-04-08 03:23:52 +0600

    leetcode problems two-sum submissions 1226159480

---
+++ algorithms/000001-two-sum/solution.php
@@ -0,0 +1,19 @@
+class Solution {
+
+/**
+ * @param Integer[] $nums
+ * @param Integer $target
+ * @return Integer[]
+ */
+function twoSum($nums, $target) {
+    $ind = [];
+    for ($i = 0; $i < count($nums); ++$i) {
+        $complement = $target - $nums[$i];
+        if (array_key_exists($complement, $ind)) {
+            return [$ind[$complement], $i];
+        }
+        $ind[$nums[$i]] = $i;
+    }
+    return [-1, -1];
+}
+}
