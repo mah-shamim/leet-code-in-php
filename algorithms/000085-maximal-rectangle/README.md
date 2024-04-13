378b1361a3a5e94fd4c2dbf1151587a9cc7c243a
MD ARIFUL HAQUE <mah.shamim@gmail.com>
2024-04-14 00:55:39 +0600

    leetcode problems maximal-rectangle submissions 1231489922

---
+++ algorithms/000085-maximal-rectangle/README.md
@@ -0,0 +1,30 @@
+85\. Maximal Rectangle
+
+Hard
+
+Given a `rows x cols` binary matrix filled with `0`'s and `1`'s, find the largest rectangle containing only `1`'s and return <i>its area</i>.
+
+**Example 1:**
+
+![](https://assets.leetcode.com/uploads/2020/09/14/maximal.jpg)
+
+- **Input:** <code>**matrix = [["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]**</code>
+- **Output:** <code>**6**</code>
+- **Explanation:** <code>**The maximal rectangle is shown in the above picture**</code>.
+
+**Example 2:**
+
+- **Input:** <code>**matrix = [["0"]]**</code>
+- **Output:** <code>**0**</code>
+
+**Example 3:**
+- **Input:** <code>**matrix = [["1"]]**</code>
+- **Output:** <code>**1**</code>
+
+
+**Constraints:**
+
+*   `rows == matrix.length`
+*   `cols == matrix[i].length`
+*   `1 <= row, cols <= 200`
+*   <code>`matrix[i][j]` is `'0'` or `'1'`</code>.
---
+++ algorithms/000085-maximal-rectangle/solution.php
@@ -0,0 +1,46 @@
+class Solution {
+
+/**
+ * @param String[][] $matrix
+ * @return Integer
+ */
+function maximalRectangle($matrix) {
+    if (!$matrix) {
+        return 0;
+    }
+
+    $result = 0;
+    $m = count($matrix);
+    $n = count($matrix[0]);
+    $L = array_fill(0, $n, 0);
+    $H = array_fill(0, $n, 0);
+    $R = array_fill(0, $n, $n);
+
+    for ($i = 0; $i < $m; $i++) {
+        $left = 0;
+        for ($j = 0; $j < $n; $j++) {
+            if ($matrix[$i][$j] == '1') {
+                $L[$j] = max($L[$j], $left);
+                $H[$j] += 1;
+            } else {
+                $L[$j] = 0;
+                $H[$j] = 0;
+                $R[$j] = $n;
+                $left = $j + 1;
+            }
+        }
+
+        $right = $n;
+        for ($j = $n - 1; $j >= 0; $j--) {
+            if ($matrix[$i][$j] == '1') {
+                $R[$j] = min($R[$j], $right);
+                $result = max($result, $H[$j] * ($R[$j] - $L[$j]));
+            } else {
+                $right = $j;
+            }
+        }
+    }
+
+    return $result;
+}
+}
