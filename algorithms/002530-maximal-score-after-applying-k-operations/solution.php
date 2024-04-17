b0cb40752af6789f5aafce30d3c72d2ac2b652a5
MD ARIFUL HAQUE <mah.shamim@gmail.com>
2024-04-18 01:47:04 +0600

    leetcode problems php solution  maximal-score-after-applying-k-operations submissions 1235186281

--- algorithms/002530-maximal-score-after-applying-k-operations/solution.php
+++ algorithms/002530-maximal-score-after-applying-k-operations/solution.php
@@ -1,15 +1,25 @@
 <?php
 
-class Solution
-{
+class Solution {
 
     /**
      * @param Integer[] $nums
      * @param Integer $k
      * @return Integer
      */
-    function maxKelements(array $nums, int $k)
+    function maxKelements(array $nums, int $k): int
     {
-
+        $pq = new SplMaxHeap(); // Building Max Heap
+        $ans = 0;
+        foreach($nums as $num){
+            $pq->insert($num);
+        }
+        $sum = 0;
+        for($i=0; $i<$k; $i++){
+            $t = $pq->extract();
+            $sum += (int)$t;
+            $pq->insert((int)($t+2)/3); // taking ceil value
+        }
+        return $sum;
     }
 }
