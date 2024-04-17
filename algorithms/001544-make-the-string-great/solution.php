35448413fbae700674d21f2b946db1cd3aa50864
MD ARIFUL HAQUE <mah.shamim@gmail.com>
2024-04-18 01:04:34 +0600

    leetcode problems make-the-string-great submissions 1235154388

---
+++ algorithms/001544-make-the-string-great/solution.php
@@ -0,0 +1,26 @@
+<?php
+
+class Solution {
+
+    /**
+     * @param String $s
+     * @return String
+     */
+    function makeGood(string $s): string
+    {
+        $ans = [];
+        foreach(str_split($s) as $c) {
+            if (!empty($ans) && $this->isBadPair(end($ans), $c)) {
+                array_pop($ans);
+            } else {
+                $ans[] = $c;
+            }
+        }
+        return implode('', $ans);
+    }
+
+    private function isBadPair($a, $b): bool
+    {
+        return $a != $b && strtolower($a) == strtolower($b);
+    }
+}
