060f673f198eddd0ac3b4ec659047112bcf4f45b
MD ARIFUL HAQUE <mah.shamim@gmail.com>
2024-06-27 00:20:18 +0600

    leetcode problems balance-a-binary-search-tree submissions 1301195907

---
+++ algorithms/001382-balance-a-binary-search-tree/solution.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($val = 0, $left = null, $right = null) {
+ *         $this->val = $val;
+ *         $this->left = $left;
+ *         $this->right = $right;
+ *     }
+ * }
+ */
+class Solution {
+
+    /**
+     * @param TreeNode $root
+     * @return TreeNode
+     */
+    function balanceBST($root) {
+        $nums = [];
+        $this->inorder($root, $nums);
+        return $this->build($nums, 0, count($nums) - 1);
+    }
+
+    function inorder($root, &$nums) {
+        if ($root == null)
+            return;
+        $this->inorder($root->left, $nums);
+        $nums[] = $root->val;
+        $this->inorder($root->right, $nums);
+    }
+
+    function build($nums, $l, $r) {
+        if ($l > $r)
+            return null;
+        $m = (int)(($l + $r) / 2);
+        return new TreeNode($nums[$m], $this->build($nums, $l, $m - 1), $this->build($nums, $m + 1, $r));
+    }
+}
