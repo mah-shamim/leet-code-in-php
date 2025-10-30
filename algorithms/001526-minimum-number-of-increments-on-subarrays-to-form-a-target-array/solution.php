14a630835d51d52673d09d566c941332508f572b
MD ARIFUL HAQUE <mah.shamim@gmail.com>
2025-10-30 10:30:52 +0600

    Automated commit 1

---
+++ algorithms/001526-minimum-number-of-increments-on-subarrays-to-form-a-target-array/solution.php
@@ -0,0 +1,19 @@
+<?php
+
+class Solution {
+
+    /**
+     * @param Integer[] $target
+     * @return Integer
+     */
+    function minNumberOperations($target) {
+        $operations = $target[0];
+        $n = count($target);
+        for ($i = 1; $i < $n; $i++) {
+            if ($target[$i] > $target[$i - 1]) {
+                $operations += $target[$i] - $target[$i - 1];
+            }
+        }
+        return $operations;
+    }
+}
